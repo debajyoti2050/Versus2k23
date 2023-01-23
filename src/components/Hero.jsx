ae493d4709656530c4be5c79c42b5c817f602ce6
Pritam Das <pritamstudy123@gmail.com>
2023-01-24 00:25:36 +0530

    hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
 import About from './About'
+import "../styles/Hero.css"
+import BgVideo from "../assets/bg.mp4"
 
 const Hero = () => {
   return (
     <>
+    <header>
+      <div className="hero-video flex">
+        <p>Let the War Begin</p>
+        <h1>Versus</h1>
+        <a href="#" className="primary-button">Register Now</a>
+        <video className="video-bg" autoPlay loop muted>
+          <source src={BgVideo} type="video/mp4" />
+        </video>
+      </div>
+    </header>
     <About />
     </>
   )
