9bc4486ff76da688c95739723d337d8cf1bc5132
Debajyoti Bhattacharjee <debajyoti0007@gmail.com>
2023-01-26 01:35:53 +0530

    navbar and google button

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,50 +1,69 @@
-import React, { useState } from 'react'
-import About from './About'
-import "../styles/Hero.css"
-import BgVideo from "../assets/bg.mp4"
-import { GoogleAuthProvider, onAuthStateChanged, signInWithPopup, signOut } from 'firebase/auth'
-import { auth } from '../firebase'
-import { useEffect } from 'react'
+import React, { useState } from "react";
+import About from "./About";
+import "../styles/Hero.css";
+import "../styles/hero.scss";
+import BgVideo from "../assets/bg.mp4";
+import {
+  GoogleAuthProvider,
+  onAuthStateChanged,
+  signInWithPopup,
+  signOut,
+} from "firebase/auth";
+import { auth } from "../firebase";
+import { useEffect } from "react";
 
 const Hero = () => {
-  const [user, setUser] = useState(null)
+  const [user, setUser] = useState(null);
 
   const handleGoogleSignIn = () => {
-    const googleProvider = new GoogleAuthProvider()
+    const googleProvider = new GoogleAuthProvider();
     signInWithPopup(auth, googleProvider)
-    .then(async (result) => {
-      console.log(result);
-    })
-    .catch((error) => {
-      console.log(error);
-    })
-  }
+      .then(async (result) => {
+        console.log(result);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
-  useEffect (() => {
+  useEffect(() => {
     onAuthStateChanged(auth, (user) => {
       if (user) setUser(user);
       else setUser(null);
-      });
-    }, [user])  
-
+    });
+  }, [user]);
 
   return (
     <>
-    <header>
-      <div className="hero-video flex">
-        <p>Let the War Begin</p>
-        <h1>Versus</h1>
-        { user ? <h3>hello {user.displayName} </h3> : <button onClick={handleGoogleSignIn} className="primary-button">Register Now</button>}
-        {user ? <button onClick={() => signOut(auth)} className="primary-button">Sign Out</button> : <></>}
-         {/* <button onClick={handleGoogleSignIn} className="primary-button">Register Now</button> */}
-        <video className="video-bg" autoPlay loop muted>
-          <source src={BgVideo} type="video/mp4" />
-        </video>
-      </div>
-    </header>
-    <About />
+      <header>
+        <div className="hero-video flex">
+          <p>Let the War Begin</p>
+          <h1>Versus</h1>
+          {user ? (
+            <h3>hello {user.displayName} </h3>
+          ) : (
+           
+              <button onClick={handleGoogleSignIn} type="button" class="login-with-google-btn">
+                Sign in with Google
+              </button>
+           
+          )}
+          {user ? (
+            <button onClick={() => signOut(auth)} className="primary-button">
+              Sign Out
+            </button>
+          ) : (
+            <></>
+          )}
+          {/* <button onClick={handleGoogleSignIn} className="primary-button">Register Now</button> */}
+          <video className="video-bg" autoPlay loop muted>
+            <source src={BgVideo} type="video/mp4" />
+          </video>
+        </div>
+      </header>
+      <About />
     </>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
