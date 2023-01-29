6fd6678c26031c2470a583bc0bf46f64a6f5be51
Debajyoti Bhattacharjee <debajyoti0007@gmail.com>
2023-01-29 22:34:05 +0530

    profile

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { Route, Routes } from "react-router";
 import Navbar from "./components/Navbar";
+import Profile from "./components/Profile";
 import Home from "./pages/Home";
 
 function App() {
   return (
    <Routes>
     <Route exact path="/" element={<Home />} />
+    <Route path='/profile' element={<Profile />} />
    </Routes>
   );
 }
