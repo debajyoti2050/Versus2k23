24038bfff6b6cfc62de0c298c5399827e2e15910
Pritam Das <pritamstudy123@gmail.com>
2023-02-05 03:20:27 +0530

    FAIL

--- src/pages/RegistrationFail.jsx
+++ src/pages/RegistrationFail.jsx
@@ -10,12 +10,12 @@ const RegistrationFail = () => {
     <Navbar/>
     <div class="container">
     <div class="text-center mt-5">
-        <img class="profileimg" src={ErrorIMG} alt='' />
+        <img class="profileimg2" src={ErrorIMG} alt='' />
     </div>
     <p class="text-center T-text mb-4">Oops!</p>
         <p class="text-center T-subtext">Something went wrong.</p>
     <div class="text-center"><a href="/">
-    <div class="btn prof-btn pr-4 pl-4 mt-5">Go Home</div></a>
+    <div class="btn prof-but pr-4 pl-4 mt-5 mb-5"><i class="bi bi-arrow-up-left"></i>&nbsp;Back to Home</div></a>
     </div>
 </div>
 <Footer/>
