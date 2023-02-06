353192dde2a8dbb02cbc075f67f8939d4417f74e
Debajyoti Bhattacharjee <debajyoti0007@gmail.com>
2023-02-06 21:29:21 +0530

    bug fixed

--- src/pages/RegistrationFail.jsx
+++ src/pages/RegistrationFail.jsx
@@ -1,6 +1,8 @@
-import { signOut } from "firebase/auth";
-import React from "react";
+import { setUserId } from "firebase/analytics";
+import { onAuthStateChanged, signOut } from "firebase/auth";
+import React, { useEffect, useState } from "react";
 import { SignNoLeftTurn } from "react-bootstrap-icons";
+import { useNavigate } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import ErrorIMG from "../assets/FailureImg.png";
 import Footer from "../components/Footer";
@@ -9,6 +11,22 @@ import { auth } from "../firebase";
 import "../styles/reg-success-form.css";
 
 const RegistrationFail = () => {
+  const [user, setUser] = useState(null);
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setUserId(user);
+        // console.log(user);
+      } else {
+        navigate("/");
+      }
+    });
+  }, [navigate, user]);
   return (
     <>
       <Navbar />
@@ -21,8 +39,8 @@ const RegistrationFail = () => {
         <p class="text-center T-subtext">Please <span style={{color:"red", fontWeight:"700"}}>sign out</span> and try to <span style={{color:"green", fontWeight:"700"}}>sign in</span> again</p>
         
         <div class="text-center">
-          <a href="/">
-            <div class="btn prof-but pr-4 pl-4 mb-5" onclick= {() =>signOut(auth)}>
+          <a onClick= {() =>signOut(auth)} >
+            <div class="btn prof-but pr-4 pl-4 mb-5" >
               Sign Out
             </div>
           </a>
